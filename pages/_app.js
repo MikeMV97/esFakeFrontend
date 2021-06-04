36903527c4227a33c8410539394d5b86d5e56c60
edevars <e.dev.devars@gmail.com>
2021-06-03 21:56:05 -0500

    :sparkles: Adding site title

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,7 @@ function MyApp({ Component, pageProps }) {
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&family=Proza+Libre:wght@400;500;700&display=swap" rel="stylesheet" />
                 <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"></link>
+                <title>Es Fake</title>
             </Head>
             <ThemeProvider theme={theme} >
                 <GloabalStyles />
