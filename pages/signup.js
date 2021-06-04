5e972619c1a915064f44f069e4c2265cdc9d737b
edevars <e.dev.devars@gmail.com>
2021-06-03 21:55:35 -0500

    :bug: Changing component name

--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,7 @@
 import { AuthMessage, AuthWrapper, FormWrapper } from "../styles/AuthStyles";
 import { SignupForm } from "../components/SignupForm";
 
-const Login = () => {
+const Signup = () => {
   return (
     <AuthWrapper>
       <AuthMessage>
@@ -20,4 +20,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
