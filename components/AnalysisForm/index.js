2c733eb6a4da5e5b509b37bb0090c2ff28ca206b
edevars <e.devars.dev@gmail.com>
2021-08-18 22:07:42 -0500

    :sparkles: Adding route to report in button

--- components/AnalysisForm/index.js
+++ components/AnalysisForm/index.js
@@ -1,20 +1,27 @@
+import { useRouter } from "next/router";
 import { StyledForm } from "./styles.js";
 
-
 export const AnalysisForm = () => {
+  const router = useRouter();
+
+  const handleSubmitToResults = (e) => {
+    e.preventDefault();
+    router.push("/report");
+  };
+
   return (
-    <StyledForm action="">
+    <StyledForm action="submit" onSubmit={handleSubmitToResults}>
       <label htmlFor="title">
         <span>Título de la noticia</span>
         <input className="text" type="text" id="title" />
       </label>
       <label htmlFor="link">
         <span>Enlace de la noticia</span>
-        <input className="text" type="text" id="link"/>
+        <input className="text" type="text" id="link" />
       </label>
       <label htmlFor="content">
         <span>Cuerpo de la noticia</span>
-        <textarea name="content" id="content"/>
+        <textarea name="content" id="content" />
       </label>
       <input className="submit" type="submit" value="Analizar" />
     </StyledForm>
